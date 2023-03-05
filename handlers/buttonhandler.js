2598f856742eb13069a815c4517e2ff36dda6ad4
ALoafOfDvDs <86698826+ALoafOfDvDs@users.noreply.github.com>
2023-03-05 15:12:24 -0800

    button handling offloaded, extraneous code removed

---
+++ handlers/buttonhandler.js
@@ -0,0 +1,36 @@
+const { ActionRowBuilder, 
+    ButtonBuilder, ButtonStyle,
+    Client, Collection, 
+    EmbedBuilder, Events, 
+    Partials } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const env = require('dotenv');
+
+module.exports = {
+    ButtonInteraction(interaction) {
+        if (interaction.customId === 'report_seen' && interaction.message.author.id === process.env.CLIENT_ID) {
+            // clicked a button with the report seen id that was sent by my bot
+            // this means I need to edit the original ephemeral message
+            console.log('this is the button for marking a report as seen and needs to edit the original ephemeral message for the /report command');
+        }
+        if (interaction.customId === 'deliberate') {
+            // This is the button associated with the /modreport command, and needs to edit the original embed sent with this button to now 
+            // add the person who clicked to the button to the list of deliberating mods
+            const m = interaction.message;
+            const embed = m.embeds[0];
+            const buttons = m.components[0];
+            let new_embed = new EmbedBuilder()
+                .setFooter(embed.footer);
+            for (let i = 0; i < embed.fields.length; i += 1) {
+                if (i != 5) {
+                    new_embed.addFields({name: `${embed.fields[i].name}`, value: `${embed.fields[i].value}`});
+                }
+                else {
+                    new_embed.addFields({name: `${embed.fields[5].name}`, value: `${embed.fields[5].value}\n${interaction.user.tag}`});
+                }
+            }
+            m.edit({embeds:[new_embed], components:[buttons]});
+        }
+    }
+}
--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const path = require('node:path');
 const env = require('dotenv');
 env.config();
 
+
+const { ButtonInteraction } = require('./handlers/buttonhandler');
+
 const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
@@ -45,44 +48,8 @@ client.on('ready', () => {
 
 
 client.on(Events.InteractionCreate, async interaction => {
-	// if (!interaction.isChatInputCommand()) {
-	// 	console.log(interaction);
-	// 	return;
-	// }
 	if (interaction.isButton()) {
-		if (interaction => interaction.customId === 'report_seen' && interaction.message.author.id === process.env.CLIENT_ID) {
-			// clicked a button with the report seen id that was sent by my bot
-			// this means I need to edit the original ephemeral message
-			console.log('this is the button for marking a report as seen and needs to edit the original ephemeral message for the /report command');
-		}
-		if (interaction => interaction.customId === 'deliberate') {
-			// This is the button associated with the /modreport command, and needs to edit the original embed sent with this button to now 
-			// add the person who clicked to the button to the list of deliberating mods
-			const m = interaction.message;
-			console.log(m);
-			const embed = m.embeds[0];
-			console.log(embed);
-			const buttons = m.components[0];
-			console.log(buttons);
-			let new_embed = new EmbedBuilder();
-				// .setTitle(`${embed.title}`)
-				// .setDescription(`${embed.description}`);
-			for (let i = 0; i < embed.fields.length; i += 1) {
-				console.log(`${i}`);
-				console.log(`${embed.fields[i].name}`);
-				if (i != 5) {
-					new_embed.addFields({name: `${embed.fields[i].name}`, value: `${embed.fields[i].value}`});
-				}
-				else {
-					new_embed.addFields({name: `${embed.fields[5].name}`, value: `${embed.fields[5].value}\n${interaction.user.tag}`});
-				}
-			}
-			// let new_embed = EmbedBuilder.from(embed);
-			// new_embed.fields[5] = {name: `Deliberating staff`, value: `${embed.fields[5].value}${interaction.user.tag}`};
-
-			// new_embed.fields['Deliberating staff'] = {name: embed.fields['Deliberating staff'].name, value: `${embed.fields[5].value}\n${interaction.user.tag}`};
-			m.edit({embeds:[new_embed], components:[buttons]});
-		}
+		ButtonInteraction(interaction);
 	}
 	else if (interaction.isChatInputCommand()) {
 		const command = interaction.client.commands.get(interaction.commandName);
@@ -99,64 +66,12 @@ client.on(Events.InteractionCreate, async interaction => {
 			await interaction.reply({ content: 'Something fucked up homie', ephemeral: true });
 		}
 	}
+	else {
+		console.log('command received is neither slash command nor button interaction, cannot be handled');
+		console.log(interaction);
+	}
 
 });
-
-// client.on(Events.MessageReactionAdd, async (reaction, user) => {
-// 	if (reaction.partial) {
-// 		console.log("received partial message");
-// 		message.fetch().then(fullMessage => {console.log(fullMessage.content);})
-// 		.catch(error => console.log("bad fetch for partial message: ", error));
-// 	}
-// 	else {
-// 		console.log("received non-partial message");
-// 		console.log("message content = ", reaction.message);
-// 		const message = reaction.message;
-// 		if(!message.embeds[0]) {
-// 			console.log("message is not an embed");
-// 		}
-// 		else {
-// 			console.log('message has an embed');
-// 			if(message.author.id == 1077782801635618826) {
-// 				// the embed was sent by the bot
-// 				if(user.id != 1077782801635618826) {
-// 					// the reaction was not posted by the bot
-// 				}
-// 				const embed = message.embeds[0];
-// 				if (embed.footer) {
-// 					console.log("footer exists");
-// 					const embed_message = embed.footer.text;
-// 					console.log(embed_message);
-// 					let x = "";
-// 					let flag = false;
-// 					for (var i = embed_message.length; ; i--) {
-// 						const c = embed_message.charAt(i);
-// 						if (!flag){
-// 							if (c == ')') {
-// 								flag = true;
-// 							}
-// 						}
-// 						else {
-// 							if (c == '(') {
-// 								break;
-// 							}
-// 							x = c + x;
-// 						}
-// 					}
-// 					const reporter_id = parseInt(x, 10);
-// 					console.log(reporter_id);
-
-// 					// const channel = embed.fields.find(f=>f.name === "Channel").value;
-// 					// channel.send({content:`A moderator has seen your report`, ephemeral: true});
-// 					// the above command is breaking, and due to reactions used as buttons being deprecated
-// 					// I will not be trying to fix this
-// 				}
-// 				// const new_embed = EmbedBuilder.from(embed)
-// 			}
-// 		}
-// 	}
-
-// });
 
 
 console.log(process.env.DISCORD_TOKEN);
